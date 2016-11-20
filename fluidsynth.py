c71d374a541ff8dd040f39c3411ec6dccc4ce1ad
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-11-21 00:46:18 +0700

    Import the fluidsynth python wrapper from the bzamecnik/ml project.
    Moved from: https://github.com/bzamecnik/ml/blob/master/instrument-classification/fluidsynth.py

---
+++ fluidsynth.py
@@ -0,0 +1,59 @@
+"""
+Synthesizes an audio file from MIDI using some basic sound font.
+
+It's a wrapper over fluidsynth which provides a bit easier interface.
+
+Input: MIDI file
+Output: audio file (WAV, FLAC, ...) - type determined from the extension
+
+Usage in shell:
+
+# use some default sound font
+$ python fluidsynth.py input.mid output.flac
+# use a custom sound font
+$ python fluidsynth.py -s sound_font.sf2 input.mid output.flac
+
+Usage in Python:
+
+FluidSynth('sound_font.sf2').midi_to_audio('input.mid', 'output.wav')
+
+If no sound font is specified explicitly the path of a default one is searched in the `~/.fluidsynth/default_sound_font` file.
+"""
+
+import argparse
+from os.path import expanduser
+import subprocess
+
+class FluidSynth():
+    def __init__(self, sound_font=None, sample_rate=44100):
+        self.sample_rate = sample_rate
+        if sound_font is not None:
+            self.sound_font = sound_font
+        else:
+            try:
+                with(open(expanduser('~/.fluidsynth/default_sound_font'))) as f:
+                    self.sound_font = f.readline().strip()
+            except Exception as ex:
+                raise RuntimeError('Default sound font is not defined, you have to specify it explicitly') from ex
+
+    def midi_to_audio(self, midi_file, audio_file):
+        subprocess.call(['fluidsynth', '-ni', self.sound_font, midi_file, '-F', audio_file, '-r', str(self.sample_rate)])
+
+    def play_midi(self, midi_file):
+        subprocess.call(['fluidsynth', '-i', self.sound_font, midi_file, '-r', str(self.sample_rate)])
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Convert MIDI to audio via FluidSynth')
+    parser.add_argument('midi_file', metavar='MIDI', type=str)
+    parser.add_argument('audio_file', metavar='AUDIO', type=str, nargs='?')
+    parser.add_argument('-s', '--sound-font', type=str)
+    parser.add_argument('-r', '--sample-rate', type=int, nargs='?', default=44100)
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    fs = FluidSynth(args.sound_font, args.sample_rate)
+    if args.audio_file:
+        fs.midi_to_audio(args.midi_file, args.audio_file)
+    else:
+        fs.play_midi(args.midi_file)
---
+++ install_fluidsynth_with_soundfonts_osx.sh
@@ -0,0 +1,63 @@
+# Install FluidSynth MIDI synthesizer which is available for non-realtime batch
+# synthesis and some sound fonts.
+# This script works on OSX with homebrew.
+
+echo "Installing FluidSynth:"
+brew install fluid-synth --with-libsndfile
+
+# brew install p7zip
+
+echo "Installing sound fonts:"
+
+TARGET_FILE="$HOME/Library/Audio/Sounds/Banks/fluid_r3_gm.sf2"
+ARCHIVE_FILE="/tmp/fluid-soundfont.tar.gz"
+EXTRACTED_DIR="/tmp/fluid-soundfont"
+if [ ! -f ${TARGET_FILE} ]; then
+  echo "Installing Fluid R3 GM ..."
+  if [ ! -f ${ARCHIVE_FILE} ]; then
+    wget 'http://www.musescore.org/download/fluid-soundfont.tar.gz' -O ${ARCHIVE_FILE}
+  fi
+  mkdir -p ${EXTRACTED_DIR}
+  tar -xzvf ${ARCHIVE_FILE} -C ${EXTRACTED_DIR}
+  mv "${EXTRACTED_DIR}/FluidR3 GM2-2.SF2" ${TARGET_FILE}
+  rm -r ${EXTRACTED_DIR}
+else
+  echo "Fluid R3 GM is up-to-date."
+fi
+
+# let's store this sound font as default
+mkdir -p ~/.fluidsynth
+echo ${TARGET_FILE} > ~/.fluidsynth/default_sound_font
+
+# TARGET_FILE="$HOME/Library/Audio/Sounds/Banks/generaluser_gs_v1.47.sf2"
+# ARCHIVE_FILE="/tmp/GeneralUser_GS_1.47.zip"
+# EXTRACTED_DIR="/tmp/general_user_gs"
+# if [ ! -f ${TARGET_FILE} ]; then
+#   echo "Installing GeneralUser GS ..."
+#   if [ ! -f ${ARCHIVE_FILE} ]; then
+#     wget https://dl.dropboxusercontent.com/u/8126161/GeneralUser_GS_1.47.zip -O ${ARCHIVE_FILE}
+#   fi
+#   unzip -q ${ARCHIVE_FILE} -d ${EXTRACTED_DIR}
+#   mv "$EXTRACTED_DIR/GeneralUser GS 1.47/GeneralUser GS v1.47.sf2" ${TARGET_FILE}
+#   rm -r "$EXTRACTED_DIR"
+# else
+#   echo "GeneralUser GS is up-to-date."
+# fi
+#
+# TARGET_FILE="$HOME/Library/Audio/Sounds/Banks/timbres_of_heaven_v3.2_final.sf2"
+# ARCHIVE_FILE="/tmp/Timbres_Of_Heaven_GM_GS_XG_SFX_V_3.2_Final.7z"
+# EXTRACTED_DIR="/tmp/Timbres_Of_Heaven_GM_GS_XG_SFX_V_3.2_Final"
+# if [ ! -f ${TARGET_FILE} ]; then
+#   echo "Installing Timbres Of Heaven ..."
+#   if [ ! -f ${ARCHIVE_FILE} ]; then
+#     wget 'http://download906.mediafire.com/3npkfyxbm5pg/klclifq2g91o2tq/Timbres+Of+Heaven+GM_GS_XG_SFX+V+3.2+Final.7z' -O ${ARCHIVE_FILE}
+#   fi
+#   7z x -o${EXTRACTED_DIR} ${ARCHIVE_FILE}
+#   mv "${EXTRACTED_DIR}/Timbres Of Heaven GM_GS_XG_SFX V 3.2 Final.sf2" ${TARGET_FILE}
+#   rm -r ${EXTRACTED_DIR}
+# else
+#   echo "Timbres Of Heaven is up-to-date."
+# fi
+
+echo "Installed sound fonts:"
+ls -lh ~/Library/Audio/Sounds/Banks/
