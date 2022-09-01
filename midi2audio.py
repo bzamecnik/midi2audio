179ff7ad399196358f8566ad06178c5262dd1cfd
Daniel Redly <dredly@gmail.com>
2022-09-01 14:36:49 +0100

    Add ability to set gain of outputs

--- midi2audio.py
+++ midi2audio.py
@@ -36,17 +36,18 @@ __all__ = ['FluidSynth']
 
 DEFAULT_SOUND_FONT = '~/.fluidsynth/default_sound_font.sf2'
 DEFAULT_SAMPLE_RATE = 44100
+DEFAULT_GAIN = 0.2
 
 class FluidSynth():
     def __init__(self, sound_font=DEFAULT_SOUND_FONT, sample_rate=DEFAULT_SAMPLE_RATE):
         self.sample_rate = sample_rate
         self.sound_font = os.path.expanduser(sound_font)
 
-    def midi_to_audio(self, midi_file, audio_file):
-        subprocess.call(['fluidsynth', '-ni', self.sound_font, midi_file, '-F', audio_file, '-r', str(self.sample_rate)])
+    def midi_to_audio(self, midi_file, audio_file, gain=DEFAULT_GAIN):
+        subprocess.call(['fluidsynth', '-ni', '-g', str(gain), self.sound_font, midi_file, '-F', audio_file, '-r', str(self.sample_rate)])
 
-    def play_midi(self, midi_file):
-        subprocess.call(['fluidsynth', '-i', self.sound_font, midi_file, '-r', str(self.sample_rate)])
+    def play_midi(self, midi_file, gain=DEFAULT_GAIN):
+        subprocess.call(['fluidsynth', '-i', '-g', str(gain), self.sound_font, midi_file, '-r', str(self.sample_rate)])
 
 def parse_args(allow_synth=True):
     parser = argparse.ArgumentParser(description='Convert MIDI to audio via FluidSynth')
