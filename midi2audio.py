176d01638ecaab6b84f85ad68646fd3427207598
Daniel Redly <dredly@gmail.com>
2022-09-28 08:37:35 +0100

    Removed unwanted files and moved optional gain argument to __init__ of Fluidsynth class

--- midi2audio.py
+++ midi2audio.py
@@ -39,15 +39,16 @@ DEFAULT_SAMPLE_RATE = 44100
 DEFAULT_GAIN = 0.2
 
 class FluidSynth():
-    def __init__(self, sound_font=DEFAULT_SOUND_FONT, sample_rate=DEFAULT_SAMPLE_RATE):
+    def __init__(self, sound_font=DEFAULT_SOUND_FONT, sample_rate=DEFAULT_SAMPLE_RATE, gain=DEFAULT_GAIN):
         self.sample_rate = sample_rate
         self.sound_font = os.path.expanduser(sound_font)
+        self.gain = gain
 
-    def midi_to_audio(self, midi_file, audio_file, gain=DEFAULT_GAIN):
-        subprocess.call(['fluidsynth', '-ni', '-g', str(gain), self.sound_font, midi_file, '-F', audio_file, '-r', str(self.sample_rate)])
+    def midi_to_audio(self, midi_file, audio_file):
+        subprocess.call(['fluidsynth', '-ni', '-g', str(self.gain), self.sound_font, midi_file, '-F', audio_file, '-r', str(self.sample_rate)])
 
-    def play_midi(self, midi_file, gain=DEFAULT_GAIN):
-        subprocess.call(['fluidsynth', '-i', '-g', str(gain), self.sound_font, midi_file, '-r', str(self.sample_rate)])
+    def play_midi(self, midi_file):
+        subprocess.call(['fluidsynth', '-i', '-g', str(self.gain), self.sound_font, midi_file, '-r', str(self.sample_rate)])
 
 def parse_args(allow_synth=True):
     parser = argparse.ArgumentParser(description='Convert MIDI to audio via FluidSynth')
